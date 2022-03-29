1176837cc1792523f17e91db20acb4ee2a53c87b
MicheleMauriello <96469371+MicheleMauriello@users.noreply.github.com>
2022-03-29 10:39:00 +0200

    Add files via upload

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -10,12 +10,11 @@ import javax.ws.rs.core.Response;
 @Path("/book")
 public class Library {
     private final String error = "Server error, contact administrators";
-    private boolean checkParams(String isbn,String autore, String titolo){//devo creare un altro metodo checkparams
+    private boolean checkParams(String isbn,String autore, String titolo){
         return (isbn == null || isbn.trim().length() == 0) || (titolo == null || titolo.trim().length() == 0) || (autore == null || autore.trim().length() == 0);
     }
-
-    private boolean checkParams2(String idUtente,String ISBN, String dataInizio, String dataFine){
-        return (idUtente == null || idUtente.trim().length() == 0) || (ISBN == null || ISBN.trim().length() == 0) || (dataInizio == null || dataInizio.trim().length() == 0 ) || (dataFine == null || dataFine.trim().length() == 0 );
+    private boolean checkParams2(String idPrestiti, String IDLibro, String idUtente, String dataInizio, String dataFine, String ISBN){
+        return (idPrestiti == null || idPrestiti.trim().length() == 0) || (IDLibro == null || IDLibro.trim().length() == 0) || ( idUtente== null || idUtente.trim().length() == 0) || (dataInizio == null || dataInizio.trim().length() == 0 || (dataFine==null || dataFine.trim().length()==0) || (ISBN==null || ISBN.trim().length()==0));
     }
 
     @GET
@@ -136,53 +135,57 @@ public class Library {
         String obj = new Gson().toJson("Libro con ISBN:" + isbn + " eliminato con successo");
         return Response.ok(obj,MediaType.APPLICATION_JSON).build();
     }
-    
-    @PUT
-    @Path("/Prestito")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response update2(@FormParam("idUtente") String idUtente,
-                           @FormParam("ISBN")   String ISBN,
-                           @FormParam("dataInizio") String dataInizio,
-                           @FormParam("dataFine") String dataFine){
-        if(checkParams2(idUtente, ISBN, dataInizio, dataFine)) {
+
+
+@PUT
+@Path("/Prestiti")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+public Response update2(@FormParam("idPrestiti") String idPrestiti,
+                       @FormParam("IDLibro")   String IDLibro,
+                       @FormParam("idUtente")   String idUtente,
+                       @FormParam("dataInizio") String dataInizio,
+                       @FormParam("dataFine") String dataFine,
+                       @FormParam("ISBN") String ISBN){
+        if(checkParams2(idPrestiti, IDLibro, idUtente, dataInizio, dataFine, ISBN))
+        {
             String obj = new Gson().toJson("Parameters must be valid");
             return Response.serverError().entity(obj).build();
-        }
-        
-        int quantita = 0;
-        final String QUERY = "INSERT INTO Prestiti(idUtente, ISBN, dataInizio, dataFine) VALUE(?,?,?,?)";
-        final String querySelezionaQuantita = "SELECT quantita FROM Libri WHERE ISBN = '"+ISBN+"'";
+        }             
+        int totale = 0;
+        final String QUERY = "INSERT INTO Prestiti(idPrestiti, IDLibro, idUtente, dataInizio, dataFine, ISBN) VALUE(?,?,?,?)";
+        final String querySelezionaTotale = "SELECT Totale FROM Libri WHERE ISBN = '"+ISBN+"'";
         
         final String[] data = Database.getData();
         try(
 
-                Connection conn = DriverManager.getConnection(data[0]);
-                PreparedStatement pstmt = conn.prepareStatement( QUERY );
-                PreparedStatement pstmt1 = conn.prepareStatement( querySelezionaQuantita );
-        ) {
-            pstmt.setString(1, idUtente);
-            pstmt.setString(2, ISBN);
-            pstmt.setString(3, dataInizio);
-            pstmt.setString(4, dataFine);
-            pstmt.execute();
+            Connection connection = DriverManager.getConnection(data[0]);
+            PreparedStatement pstmt = connection.prepareStatement( QUERY );
+            PreparedStatement pstmt1 = connection.prepareStatement( querySelezionaTotale );
+    ) {
+        pstmt.setString(1, idPrestiti);
+        pstmt.setString(2, IDLibro);
+        pstmt.setString(3, idUtente);
+        pstmt.setString(4, dataInizio);
+        pstmt.setString(5, dataFine);
+        pstmt.setString(6, ISBN);
+        pstmt.execute();
 
-            ResultSet r = pstmt1.executeQuery();
-            while(r.next()){
-                quantita = Integer.parseInt(r.getString("quantita"));
-            }
-
-            if(quantita >0 ){
-                final String QUERYModificaQuantita = "UPDATE Libri SET quantita = quantita - '"+quantita+"'";
-                PreparedStatement pstmt2 = conn.prepareStatement( QUERYModificaQuantita );
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-            String obj = new Gson().toJson(error);
-            return Response.serverError().entity(obj).build();
+        ResultSet r = pstmt1.executeQuery();
+        while(r.next()){
+            totale = Integer.parseInt(r.getString("Totale libri"));
         }
-        String obj = new Gson().toJson("Libro con ISBN:" + ISBN + " aggiunto con successo");
-        return Response.ok(obj,MediaType.APPLICATION_JSON).build();
+        if(totale >0 ){
+            final String QUERYModificaTotale = "UPDATE Libri SET Totale = Totale - '"+totale+"'";
+            PreparedStatement pstmt2 = connection.prepareStatement( QUERYModificaTotale );
+        }
+    }catch (SQLException e){
+        e.printStackTrace();
+        String obj = new Gson().toJson(error);
+        return Response.serverError().entity(obj).build();
     }
+    String obj = new Gson().toJson("Libro con ISBN:" + ISBN + " aggiunto libro");
+    return Response.ok(obj,MediaType.APPLICATION_JSON).build();
+}
 
 }
